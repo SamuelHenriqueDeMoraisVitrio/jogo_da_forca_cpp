33a5054e2fc9f47b38f212c10669611562adfba1
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-05-06 23:58:47 -0300

    teste
    Testando o 'static_...'

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello Word" << '\0' << '\n';
+  std::vector<std::string> PALAVRA_SECRETA{"MELANCIA", "ABACATE", "MAMÃO"};
+
+  int valor_IN{static_cast<int>(*argv[1] - 0)};
+  std::cout << valor_IN << '\n';
 }
