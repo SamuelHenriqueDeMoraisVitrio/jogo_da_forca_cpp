5f90189c3d054a2d8f7f5096ddf4b3b93a7d376a
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-05-17 00:26:21 -0300

    Usando array

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
+#include <array>
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include <vector>
 
 int main(int argc, char *argv[]) {
-  std::vector<std::string> PALAVRA_SECRETA{"MELANCIA", "ABACATE", "MAMÃO"};
+  std::array<std::string, 3> PALAVRA_SECRETA{"MELANCIA", "ABACATE", "MAMÃO"};
 
-  std::cout << "\n\t" << *argv[1] << '\t' << static_cast<int>(*argv[1]) << '\t'
-            << static_cast<char>(*argv[1]) << "\n\t" << argv[1] << '\t'
-            << static_cast<int>(std::atof(argv[1])) << '\t' << static_cast<char *>(argv[1]);
+  short valor_IN = std::atoi(argv[1]);
 
-  int valor_IN{static_cast<int>(*argv[1] - 0)};
   std::cout << '\n' << '\n' << valor_IN << '\n';
 }
